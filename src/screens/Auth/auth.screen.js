55c741fbc862eb717c7089ebbd717e8fefa0b284
FinEx1906 <coderakhmedov@gmail.com>
2024-01-13 17:15:16 +0500

    add

--- src/screens/Auth/auth.screen.js
+++ src/screens/Auth/auth.screen.js
@@ -18,36 +18,6 @@ const AuthScreen = () => {
   };
   return (
     <View>
-      <View
-        className="items-center justify-center bg-gray-50 h-72 shadow-2xl"
-        style={{ borderBottomEndRadius: 50, borderBottomStartRadius: 50 }}
-      >
-        <Image
-          source={Images.BRAND}
-          style={styles.authImage}
-          className="mt-5"
-        />
-        <View className="flex-row w-full justify-evenly mt-5">
-          <TouchableOpacity>
-            <Text
-              className="font-bold text-xl tracking-wider"
-              onPress={() => TabPress("Kirish")}
-              style={[selectTab === "Kirish" && { color: "#fa4a0c" }]}
-            >
-              Kirish
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text
-              className="font-bold text-xl tracking-wider"
-              onPress={() => TabPress("Yaratish")}
-              style={[selectTab === "Yaratish" && { color: "#fa4a0c" }]}
-            >
-              Yaratish
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
       <View className="justify-center items-center w-full mt-5">
         <View>
           <View className="flex-row items-center">
@@ -84,7 +54,7 @@ const AuthScreen = () => {
               style={{ position: "absolute", zIndex: 1, left: 10 }}
             />
             <TextInput
-            secureTextEntry={true}
+              secureTextEntry={true}
               placeholder="Parolingizni kiriting"
               className="text-lg w-72 h-16 bg-gray-50 pl-10 pt-0 rounded-2xl"
               keyboardType="default"
@@ -107,11 +77,147 @@ const AuthScreen = () => {
             className="w-72 h-16 rounded-xl justify-center items-center"
           >
             <Text className="text-xl text-white font-bold tracking-widest">
-              Salom
+              Hisobga kirish
             </Text>
           </TouchableOpacity>
         </View>
       </View>
+    </View>
+  );
+};
+
+
+const RegisterScreen = () => {
+  const [selectTab, setSelecTab] = useState("Kirish");
+  const TabPress = (tab) => {
+    setSelecTab(tab);
+  };
+  return (
+    <View>
+
+      <View className="justify-center items-center w-full mt-5">
+        <View>
+          <View className="flex-row items-center">
+            <Ionicons
+              name="call-outline"
+              size={25}
+              color="#B2B2B2"
+              style={{ position: "absolute", zIndex: 1, left: 10 }}
+            />
+            <TextInput
+              placeholder="Telefon raqamingizni kiriting"
+              className="text-lg w-72 h-16 bg-gray-50 pl-10 pt-0 rounded-2xl"
+              keyboardType="phone-pad"
+            />
+          </View>
+          <View className="flex-row items-center mt-2">
+            <Ionicons
+              name="mail-outline"
+              size={25}
+              color="#B2B2B2"
+              style={{ position: "absolute", zIndex: 1, left: 10 }}
+            />
+            <TextInput
+              placeholder="Email manzilingizni kiriting"
+              className="text-lg w-72 h-16 bg-gray-50 pl-10 pt-0 rounded-2xl"
+              keyboardType="email-address"
+            />
+          </View>
+          <View className="flex-row items-center mt-2">
+            <Ionicons
+              name="key-outline"
+              size={25}
+              color="#B2B2B2"
+              style={{ position: "absolute", zIndex: 1, left: 10 }}
+            />
+            <TextInput
+              secureTextEntry={true}
+              placeholder="Parolingizni kiriting"
+              className="text-lg w-72 h-16 bg-gray-50 pl-10 pt-0 rounded-2xl"
+              keyboardType="default"
+            />
+          </View>
+          <View className="flex-row items-center mt-2">
+            <Ionicons
+              name="shield-checkmark-outline"
+              size={25}
+              color="#B2B2B2"
+              style={{ position: "absolute", zIndex: 1, left: 10 }}
+            />
+            <TextInput
+              secureTextEntry={true}
+              placeholder="Parolni qayta kiriting"
+              className="text-lg w-72 h-16 bg-gray-50 pl-10 pt-0 rounded-2xl"
+              keyboardType="default"
+            />
+          </View>
+          <View className="mt-2">
+            <TouchableOpacity>
+              <Text
+                className="text-base font-medium"
+                style={{ color: "#fa4a0c" }}
+              >
+                Parol esingizdan chiqdimi?
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ position: "absolute", bottom: "-30%" }}>
+          <TouchableOpacity
+            style={{ backgroundColor: "#fa4a0c" }}
+            className="w-72 h-16 rounded-xl justify-center items-center"
+          >
+            <Text className="text-xl text-white font-bold tracking-widest">
+              Hisob yaratish
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const Authentication = () => {
+  const [selectTab, setSelectTab] = useState("Kirish");
+
+  const TabPress = (tab) => {
+    setSelectTab(tab);
+  };
+
+  return (
+    <View>
+      <View
+        className="items-center justify-center bg-gray-50 h-72 shadow-2xl"
+        style={{ borderBottomEndRadius: 50, borderBottomStartRadius: 50 }}
+      >
+        <Image
+          source={Images.BRAND}
+          style={styles.authImage}
+          className="mt-5"
+        />
+        <View className="flex-row w-full justify-evenly mt-5">
+          <TouchableOpacity>
+            <Text
+              className="font-bold text-xl tracking-wider"
+              onPress={() => TabPress("Kirish")}
+              style={[selectTab === "Kirish" && { color: "#fa4a0c" }]}
+            >
+              Kirish
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text
+              className="font-bold text-xl tracking-wider"
+              onPress={() => TabPress("Yaratish")}
+              style={[selectTab === "Yaratish" && { color: "#fa4a0c" }]}
+            >
+              Yaratish
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {selectTab === "Kirish" ? <AuthScreen /> : <RegisterScreen />}
     </View>
   );
 };
@@ -123,4 +229,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AuthScreen;
+export default Authentication;
